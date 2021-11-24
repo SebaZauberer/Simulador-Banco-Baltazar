64069eed71c95f2301cfa7b92d511f2becd651f5
Sebastian Olate H <seba.olate.h@gmail.com>
2021-11-24 00:34:51 -0300

    ultimo ajuste antes de la entrega

--- js/logica-API.js
+++ js/logica-API.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   /* peticion AJAX por GET */
-  //Url API de la informacion de la UF
   const URLGET = "https://mindicador.cl/api/uf";
 
-  //script para hacer un llamado AJAX y traer el valor monetario y la fecha de la informacion
+  //script para hacer un llamado AJAX y traer el valor monetario y la fecha del valor de la UF (Unidad de fomento Chile)
   $.get(URLGET, function (respuesta, estado) {
     if (estado === "success") {
       let misDatos = respuesta.serie[0];
