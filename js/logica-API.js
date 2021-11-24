9b2ed770206f438b0a4a9bcbcaa606866be0751a
Sebastian Olate H <seba.olate.h@gmail.com>
2021-11-24 00:32:04 -0300

    .

--- js/logica-API.js
+++ js/logica-API.js
@@ -1,21 +1,17 @@
 $(document).ready(function () {
-/* peticion AJAX por GET */
-//Url API de la informacion de la UF
-const URLGET = "https://mindicador.cl/api/uf";
-//script para hacer un llamado AJAX y traer el valor monetario y la fecha de la informacion
+  /* peticion AJAX por GET */
+  //Url API de la informacion de la UF
+  const URLGET = "https://mindicador.cl/api/uf";
 
-  $('#btnInfoValorUf').click(function (e) { 
-    e.preventDefault();
+  //script para hacer un llamado AJAX y traer el valor monetario y la fecha de la informacion
+  $.get(URLGET, function (respuesta, estado) {
+    if (estado === "success") {
+      let misDatos = respuesta.serie[0];
+      let fechaDato = misDatos.fecha.substr(0, 10);
+      let valorDato = Math.floor(misDatos.valor);
 
-    $.get(URLGET, function (respuesta, estado) {
-      if (estado === "success") {
-        let misDatos = respuesta.serie[0];
-        let fechaDato = misDatos.fecha.substr(0,10);
-        let valorDato = Math.floor(misDatos.valor);
-        
-        $('.valor-uf').text(valorDato);
-        $('.fecha-uf').text(fechaDato);
-      }
-    });
+      $('.valor-uf').text(valorDato);
+      $('.fecha-uf').text(fechaDato);
+    }
   });
 });
